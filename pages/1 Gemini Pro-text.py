d7d290fc162c44e79ddd7f80ec75571893c9449b
elias123 <126970318+elxs10@users.noreply.github.com>
2024-02-13 00:39:09 +0530

    Update 1 Gemini Pro-text.py

--- pages/1 Gemini Pro-text.py
+++ pages/1 Gemini Pro-text.py
@@ -17,7 +17,7 @@ def text_page():
     api_key = "AIzaSyAhmX3uyVgyRc36DXy_mVmr3jhE-UbyxMk"
     # Check if the API key is provided
     if not api_key:
-        st.sidebar.error("Please enter your API key.")
+        st.sidebar.error("AIzaSyAhmX3uyVgyRc36DXy_mVmr3jhE-UbyxMk")
         st.stop()
     else:
         # Store the API key in session state
@@ -25,12 +25,14 @@ def text_page():
 
     genai.configure(api_key=api_key)
 
-    
-    # Set up the model configuration options
-    temperature = st.sidebar.slider("Temperature", 0.0, 1.0, 0.9, 0.1)
-    top_p = st.sidebar.number_input("Top P", 0.0, 1.0, 1.0, 0.1)
-    top_k = st.sidebar.number_input("Top K", 1, 100, 1)
-    max_output_tokens = st.sidebar.number_input("Max Output Tokens", 1, 10000, 2048)
+    # Create a sidebar expander for customization options
+    with st.sidebar:
+        with st.expander("Customization Options"):
+            # Set up the model configuration options
+            temperature = st.slider("Temperature", 0.0, 1.0, 0.9, 0.1)
+            top_p = st.number_input("Top P", 0.0, 1.0, 1.0, 0.1)
+            top_k = st.number_input("Top K", 1, 100, 1)
+            max_output_tokens = st.number_input("Max Output Tokens", 1, 10000, 2048)
 
     # Set up the model
     generation_config = {
@@ -39,7 +41,6 @@ def text_page():
         "top_k": top_k,
         "max_output_tokens": max_output_tokens,
     }
-
 
     safety_settings = "{}"
     safety_settings = json.loads(safety_settings)
@@ -50,11 +51,7 @@ def text_page():
         st.error("Please enter your query.")
         st.stop()
 
-
-    gemini = genai.GenerativeModel(model_name="gemini-pro",
-                                  
-                                  safety_settings=safety_settings)
-                
+    gemini = genai.GenerativeModel(model_name="gemini-pro", safety_settings=safety_settings)
 
     prompt_parts = [prompt]
     
@@ -62,13 +59,11 @@ def text_page():
         response = gemini.generate_content(prompt_parts)
         st.subheader("Gemini:")
         if response.text:
-            
             st.write(response.text)
         else:
             st.write("No output from Gemini.")
     except Exception as e:
         st.write(f"An error occurred: {str(e)}")
-
 
 # Run the Streamlit app
 if __name__ == "__main__":
